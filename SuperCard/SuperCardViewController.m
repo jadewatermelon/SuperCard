42a5ff7d63dc8177388c37f16996e93a38b15d1b
Tom Billings <w.t.billings@gmail.com>
2013-02-24 08:19:46 -0500

    Added small SetCard view to storyboard and main view controller.

--- SuperCard/SuperCardViewController.m
+++ SuperCard/SuperCardViewController.m
@@ -7,21 +7,34 @@
 //
 
 #import "SuperCardViewController.h"
+
 #import "PlayingCardView.h"
 #import "PlayingCardDeck.h"
 #import "PlayingCard.h"
 
+#import "SetCardView.h"
+#import "SetCardDeck.h"
+#import "SetCard.h"
+
 @interface SuperCardViewController ()
 @property (weak, nonatomic) IBOutlet PlayingCardView *playingCardView;
-@property (strong, nonatomic) Deck *deck;
+@property (weak, nonatomic) IBOutlet SetCardView *setCardView;
+@property (strong, nonatomic) Deck *playingCardDeck;
+@property (strong, nonatomic) Deck *setCardDeck;
 @end
 
 @implementation SuperCardViewController
 
-- (Deck *)deck
+- (Deck *)playingCardDeck
 {
-    if (!_deck) _deck = [[PlayingCardDeck alloc] init];
-    return _deck;
+    if (!_playingCardDeck) _playingCardDeck = [[PlayingCardDeck alloc] init];
+    return _playingCardDeck;
+}
+
+- (Deck *)setCardDeck
+{
+    if (!_setCardDeck) _setCardDeck = [[SetCardDeck alloc] init];
+    return _setCardDeck;
 }
 
 - (void)setPlayingCardView:(PlayingCardView *)playingCardView
@@ -31,13 +44,32 @@
     [playingCardView addGestureRecognizer:[[UIPinchGestureRecognizer alloc] initWithTarget:playingCardView action:@selector(pinch:)]];
 }
 
+- (void)setSetCardView:(SetCardView *)setCardView
+{
+    _setCardView = setCardView;
+    [self drawRandomSetCard];
+}
+
 - (void)drawRandomPlayingCard
 {
-    Card *card = [self.deck drawRandomCard];
+    Card *card = [self.playingCardDeck drawRandomCard];
     if ([card isKindOfClass:[PlayingCard class]]) {
         PlayingCard *playingCard = (PlayingCard *)card;
         self.playingCardView.rank = playingCard.rank;
         self.playingCardView.suit = playingCard.suit;
+    }
+}
+
+- (void)drawRandomSetCard
+{
+    Card *card = [self.setCardDeck drawRandomCard];
+    if ([card isKindOfClass:[SetCard class]]) {
+        SetCard *setCard = (SetCard *)card;
+        self.setCardView.number = setCard.number;
+        self.setCardView.number = 1;
+        self.setCardView.symbol = 1;//setCard.symbol;
+        self.setCardView.shading = 1;//setCard.shading;
+        self.setCardView.color = 1;//setCard.color;
     }
 }
 
